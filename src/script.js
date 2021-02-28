cf793569ce2b1032beaa8b5a0fd00c567d37b825
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-02-28 20:20:09 +0100

    setting task id's and classes as they are being created

--- src/script.js
+++ src/script.js
@@ -2,22 +2,24 @@ function countTasks(){
     let listItems = document.querySelectorAll("li");
     if (tasks.length > 0){
         document.querySelector("#task-number").innerHTML = listItems.length+1;
+        return listItems.length+1;
     } else {document.querySelector("#task-number").innerHTML = 0;}
-        //document.querySelectorAll("li").forEach();
 }
-function createNewTask(content){
+function createNewTask(content, number){
     let li = document.createElement("li");
     document.querySelector("#task-list").appendChild(li);
-    li.innerHTML = `<button class="check"><i class="fas fa-check"></button></i>${content}<hr />`;
-    
+    li.innerHTML = `<button class="check${number}" id="check-${number}"><i class="fas fa-check"></button></i>${number}#:${content}<hr />`;
+    let identity = `task-${number}`;
+    li.setAttribute(id, identity);
+    li.classList.add(`task${number}`);
 }
 function handleCreate(event){
     event.preventDefault();
     let newTask = document.querySelector("#new-task-input").value;
     if (newTask.length >= 1){
         tasks.push(newTask);
-        countTasks();
-        createNewTask(newTask);
+        let taskNumber = countTasks();
+        createNewTask(newTask, taskNumber);
     }
 }
 function deleteTasks(event){
