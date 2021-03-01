24aa35c670a1c566c0beac13941bd1a04ecb54de
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-03-01 12:26:00 +0100

    "compleated task" interaction

--- src/script.js
+++ src/script.js
@@ -1,17 +1,28 @@
 function countTasks(){
-    let listItems = document.querySelectorAll("li");
+    if (completedTasks.length > 0){
+        document.querySelector("#completed-task-number").innerHTML = completedTasks.length;
+    } else {document.querySelector("#completed-task-number").innerHTML = 0;}
     if (tasks.length > 0){
-        document.querySelector("#task-number").innerHTML = listItems.length+1;
-        return listItems.length+1;
+        document.querySelector("#task-number").innerHTML = tasks.length;
+        return tasks.length+1;
     } else {document.querySelector("#task-number").innerHTML = 0;}
+}
+function handleCompleted(event) {
+    this.classList.add("checked");
+    let number = this.id.replace(`check-`,'');
+    let compleatedTask = document.querySelector(`#task-${number}`);
+    completedTasks.push(compleatedTask);
+    compleatedTask.classList.add("completed");
+    countTasks();
 }
 function createNewTask(content, number){
     let li = document.createElement("li");
     document.querySelector("#task-list").appendChild(li);
-    li.innerHTML = `<button class="check check${number}" id="check-${number}"><i class="fas fa-check"></button></i>${number}: ${content}<hr />`;
+    li.innerHTML = `<button class="check check${number}" id="check-${number}"><i class="fas fa-check"></button></i>#${number}) ${content}<hr />`;
     let identity = `task-${number}`;
     li.setAttribute(`id`, identity);
     li.classList.add(`task${number}`);
+    document.querySelector(`#check-${number}`).addEventListener("click", handleCompleted);
 }
 function handleCreate(event){
     event.preventDefault();
@@ -26,9 +37,11 @@ function deleteTasks(event){
     event.preventDefault();
     document.querySelector("#task-list").innerHTML = null;
     tasks.length = 0;
+    completedTasks.length = 0;
     countTasks();
 }
 let tasks = [];
+let completedTasks = [];
 let title = "My To-Do List";
 document.querySelector("#title").innerHTML = title;
 document.querySelector("#new-task-btn").addEventListener("click", handleCreate);
